1efe96d641c5ef14be536f8e83f6b18067ddf59a
mateUo <janowicz.mateusz@gmail.com>
2018-04-25 08:05:13 +0200

    some changes in vars

--- vars.sh
+++ vars.sh
@@ -1,39 +1,43 @@
-#definition of variables:
+#definition of variables and functions:
 
 re1='[0-9]'
 re2='[a-z]'
 re3='[A-Z]'
 args=$@
 
-if [[ $@ =~ $re1 ]] && [[ $@ =~ $re2 ]] || [[ $@ =~ $re3 ]]; then
-
+error() {
 	printf "You are not allowed to mix the arg types, enter either only integers or strings\n"
 	exit 1
+}
 
-elif [[ ! $@ =~ $re1 ]]; then
-	printf "\nArgs are not numbers."
-	printf " They are:\n\n"
-
+iter() {
 	for i in ${args[@]}
 	do
 		echo $i
 	done
-	printf "\n"
-else
-	printf "\nArgs are all numbers and I will increment them by 1.\n"
-	printf "Now our args are:\n\n"
+}
 
-        for i in ${args[@]}
-	do 
-		echo $i
-        done
-
-	printf "\nAfter incrementation:\n\n"
-
+iter2() {
 	for i in ${args[@]}
 	do
 		i=$((i+1))
 		echo $i
 	done
+}
+
+if [[ $@ =~ $re1 ]] && [[ $@ =~ $re2 ]] || [[ $@ =~ $re3 ]]; then
+	error
+
+elif [[ ! $@ =~ $re1 ]]; then
+	printf "\nArgs are not numbers."
+	printf " They are:\n\n"
+        iter
+	printf "\n"
+else
+	printf "\nArgs are all numbers and I will increment them by 1.\n"
+	printf "Now our args are:\n\n"
+        iter
+	printf "\nAfter incrementation:\n\n"
+        iter2
 	printf "\n"
 fi
