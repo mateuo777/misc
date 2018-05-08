7a9733bc54d8d48d618c9ee00914b5b424c54c91
mateUo <janowicz.mateusz@gmail.com>
2018-05-08 11:29:46 +0200

    some modifications

--- vars.sh
+++ vars.sh
@@ -47,12 +47,12 @@ elif [[ -z $@ ]]; then
 	warning
 
 elif [[ ! $@ =~ $re1 ]]; then
-	printf "\nArgs are not numbers."
-	printf " They are:\n\n"
+	printf "\nYou've entered $(echo $#) args and they are not numbers."
+	printf "They are:\n\n"
         iter
 	printf "\n"
 else
-	printf "\nArgs are all numbers and I will increment them.\n"
+	printf "\nYou've entered $(echo $#) args and the args are all numbers and I will increment them.\n\n"
 	read -p "Enter now a value, that will be used to increment the args (from 1 to 9): " -n 1 -t 10 -e value
 	printf "Now our args are:\n\n"
         iter
